1db7a3325a18d772c8de145b0c49595706027dd7
m-hiroaki <m.hiroaki1217@gmail.com>
2020-06-21 21:09:53 +0900

    テストプロジェクト追加

---
+++ SampleUwpAppTest/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleUwpApp.ViewModels;
+
+namespace SampleUwpAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ボタンクリックイベントのテスト()
+        {
+            var printerStatus = new MainPageViewModel();
+
+            // 初回のボタンクリックで"No Error"が表示される
+            Assert.AreEqual(printerStatus.ButtonClick(), "No Error");
+
+
+            // 2回目のボタンクリックで"DoorOpen"が表示される
+            Assert.AreEqual(printerStatus.ButtonClick(), "DoorOpen");
+        }
+    }
+}
